7d8b4504aba25a88dfe4638c042923141943c42d
root <root@localhost.localdomain>
2017-02-04 21:23:52 +0100

    Added post number and date to main page

--- src/wp-content/themes/fukasawa/content-image.php
+++ src/wp-content/themes/fukasawa/content-image.php
@@ -3,6 +3,10 @@
 	<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	
 		<?php if ( has_post_thumbnail() ) : ?>
+                    <div class="posts-top">
+                        #<?php the_ID();?>, <?php the_time(get_option('date_format')); ?>
+                    </div>
+            
 		
 			<div class="featured-media">	
 				
