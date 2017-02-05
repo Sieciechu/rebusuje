8e62ca5130796a6596e30a0950b07aa0ada31bf0
Sieciechu <wojciech.mocek@o2.pl>
2017-02-05 14:47:10 +0100

    rebus checking now case insensitive

--- src/wp-content/plugins/wp-rebus/rebus_single.js
+++ src/wp-content/plugins/wp-rebus/rebus_single.js
@@ -6,6 +6,8 @@
         var correctAnswer = window.atob(
             rebus.dataset.answer
         );
+        correctAnswer = correctAnswer.toLowerCase();
+        rebus.value = rebus.value.trim().toLowerCase();
         
         alert('Twoja odpowiedź jest ' + ((rebus.value != correctAnswer) ? 'nieprawidłowa' : 'prawidłowa'));
             
