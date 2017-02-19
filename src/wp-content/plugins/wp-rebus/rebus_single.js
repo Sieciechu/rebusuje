453cafb4e9d315ad1e2ca49a12276c6efd6d6539
Sieciech <wojciech.mocek@o2.pl>
2017-02-19 20:01:57 +0100

    Changed jQuery to $. Added "check answer correctness" feature

--- src/wp-content/plugins/wp-rebus/rebus_single.js
+++ src/wp-content/plugins/wp-rebus/rebus_single.js
@@ -1,16 +1,13 @@
-
-(function(){
-    jQuery('#check_rebus').click(function(){
+(function($){  
+    
+    $('#check_rebus').click(function(){
         
-        var rebus = document.getElementById('rebus');
-        var correctAnswer = decodeThePassword(rebus.dataset.answer).toLowerCase();
-            
-        rebus.value = rebus.value.trim().toLowerCase();
-        
-        alert('Twoja odpowiedź jest ' + ((rebus.value != correctAnswer) ? 'nieprawidłowa' : 'prawidłowa'));
+        var rebus = $('#rebus');
+        var correctAnswer = decodeThePassword(rebus.data('answer')).toLowerCase();
+        rebus.val(rebus.val().trim().toLowerCase());
+        setInputCssAnswerClass(rebus, rebus.val() == correctAnswer);
             
     });
-    
     
     /**
      * 
@@ -21,4 +18,23 @@
         return decodeURI(window.atob($string));
     }
     
-})();
+    /**
+     * 
+     * @param {jQuery} $input
+     * @param {bool} $isCorrect
+     * @returns {void}
+     */
+    function setInputCssAnswerClass($input, $isCorrect){
+        var classToToggle = ($isCorrect) ? 'correctAnswer' : 'badAnswer';
+        
+        var toogleClassWithFadeIn = function($jqElement, classNameToToggle){
+            $jqElement.toggleClass(classNameToToggle).fadeIn(500);
+        };
+        
+        toogleClassWithFadeIn($input, classToToggle);
+
+        setTimeout(function(){
+            toogleClassWithFadeIn($input, classToToggle);
+        }, 2000);
+    }
+})(jQuery);
